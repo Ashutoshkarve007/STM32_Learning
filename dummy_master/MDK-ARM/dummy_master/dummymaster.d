c203b6b65c00c227f1633115071f0690216c6ae6
Ashutoshkarve007 <ashutoshkarve007@gmail.com>
2022-09-13 11:42:12 +0530

    crc prog

--- dummy_master/MDK-ARM/dummy_master/dummymaster.d
+++ dummy_master/MDK-ARM/dummy_master/dummymaster.d
@@ -27,3 +27,4 @@ dummy_master\dummymaster.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_fl
 dummy_master\dummymaster.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h
 dummy_master\dummymaster.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h
 dummy_master\dummymaster.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h
+dummy_master\dummymaster.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
